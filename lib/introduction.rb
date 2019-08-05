497c34d03c9189659afa21928d50211db402aeb5
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-05 00:18:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,13 @@
-def #introduction(name)
+def introduction(name)
   puts "Hi, my name is #{name}."
 
 end
-def #introduction_with_language(name, language)
+def introduction_with_language(name, language)
   
    "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def #introduction_with_language_optional(name, language = "Ruby")
+def introduction_with_language_optional(name, language = "Ruby")
    
    "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
